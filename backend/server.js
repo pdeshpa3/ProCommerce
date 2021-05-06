780faa97acbc2352ec6002d9be7bf752409b9572
pdeshpa3 <55158700+pdeshpa3@users.noreply.github.com>
2021-05-05 20:23:01 -0400

    Cart Implementation
    Created Cart Screen and Added Cart Functionalities

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const data =  require('./data.js');
+import express from 'express';
+import cors from 'cors';
+import data from './data';
+
 const app = express();
 
 app.use(cors());
@@ -9,6 +10,16 @@ app.get('/api/products',(req, res) => {
 
     res.send(data.products);
 
+});
+
+app.get('/api/products/:id',(req, res)=>{
+    const product = data.products.find(x=>x._id === req.params.id);
+    if(product){
+        res.send(product);
+    }else{
+        res.status(404).send({message:'Product Not Found!'});
+    }
+    
 });
 
 app.listen(5000, ()=>{
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,20 +1,24 @@
+import axios from 'axios';
+import Rating from '../components/Rating';
 
 const HomeScreen = {
     render: async() =>{ 
-        const response = await fetch("http://localhost:5000/api/products", {
+        const response = await axios({
+            url:'http://localhost:5000/api/products',
             headers:{
                 "Content-Type":"application/json",
 
             },
 
         });
-        if(!response || !response.ok) {
+        if(!response || response.statusText !== 'OK') {
             return  `<div>Error in getting data</div>`;
         }
-        const products = await response.json();
+        const products = response.data;
 
         return `
         <ul class="products">
+
             ${products.map( 
                 (product) => `
             <li>
@@ -23,9 +27,14 @@ const HomeScreen = {
                         <img src="${product.image}" alt="${product.name}" />
                     </a>
                     <div class="product-name">
+
                         <a href="/#/product/1">
                             ${product.name}
                         </a>
+                    </div>
+
+                    <div class="product-rating">
+                        ${Rating.render({value: product.rating, text:`${product.numReviews} reviews`,})}
                     </div>
                 
                     <div class="product-brand">
