6b20cc3eca23b7461d563a57cc1e3782845124aa
pdeshpa3 <55158700+pdeshpa3@users.noreply.github.com>
2021-05-09 18:36:09 -0400

    dashboard 2

--- frontend/src/screens/DashboardScreen.js
+++ frontend/src/screens/DashboardScreen.js
@@ -1,9 +1,37 @@
+import Chartist from 'chartist';
 import DashboardMenu from '../components/DashboardMenu';
 import { getSummary } from '../api';
 
 let summary = {};
 const DashboardScreen = {
-  after_render: () => {},
+  after_render: () => {
+    // eslint-disable-next-line no-new
+    new Chartist.Line(
+      '.ct-chart-line',
+      {
+        labels: summary.dailyOrders.map((x) => x._id), // mapping dates as we have date in our id
+        series: [summary.dailyOrders.map((x) => x.sales)],
+      },
+      {
+        showArea: true,
+      }
+    );
+    // eslint-disable-next-line no-new
+    new Chartist.Pie(
+      '.ct-chart-pie',
+      {
+        labels: summary.productCategories.map((x) => x._id),
+        series: summary.productCategories.map((x) => x.count),
+      },
+      {
+        donut: true,
+        donutWidth: 60,
+        startAngle: 270,
+        showLabel: true,
+        donutSolid: true,
+      }
+    );
+  },
 
   render: async() => {
 
@@ -34,6 +62,18 @@ const DashboardScreen = {
             <div class="summary-body">$${summary.orders[0].totalSales}</div>
           </li>
         </ul>
+
+        <div class="charts">
+          <div>
+            <h2>Sales</h2>
+            <div class="ct-perfect-fourth ct-chart-line"></div>
+          </div>
+          <div>
+            <h2>Categories</h2>
+            <div class="ct-perfect-fourth ct-chart-pie"></div>
+          </div>
+        </div>          
+        
       </div>
     </div>
     `;
